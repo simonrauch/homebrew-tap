6b20716bd227c150b8107cff61f65fb7310e296f
Simon Rauch <simonrauch@users.noreply.github.com>
2022-03-09 00:29:32 +0100

    harvey 2.0.11

--- Formula/harvey.rb
+++ Formula/harvey.rb
@@ -1,9 +1,9 @@
 class Harvey < Formula
   desc "A simple CLI for Harvest."
   homepage "https://github.com/simonrauch/harvey"
-  url "https://github.com/simonrauch/harvey/releases/download/v2.0.10/harvey-macos-x64.tar.gz"
-  sha256 "ce1cee4c8d49cdd6a0891eee3487dad5c69b99fe562c2c504894b3fda784b98e"
-  version "2.0.10"
+  url "https://github.com/simonrauch/harvey/releases/download/v2.0.11/harvey-macos-x64.tar.gz"
+  sha256 "f3b3b4cd0f60a2d7ab7629381cd1d897c3c8f43e6e0f963fd17dce4b613a8482"
+  version "2.0.11"
   def install
     bin.install "harvey"
   end
