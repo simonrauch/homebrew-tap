c847a4ec075d46f439e4a6c03e148b02788c649c
Simon Rauch <simonrauch@users.noreply.github.com>
2022-04-27 21:15:28 +0200

    harvey 2.5.3

--- Formula/harvey.rb
+++ Formula/harvey.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Harvey < Formula
   desc "A simple CLI for Harvest"
   homepage "https://github.com/simonrauch/harvey#readme"
-  url "https://registry.npmjs.org/@simonrauch/harvey/-/harvey-2.5.2.tgz"
-  sha256 "b838772b52a3a578e5cc962dd4e58246b3cc6df2aa222490caf78ccbce1ec5b9"
+  url "https://registry.npmjs.org/@simonrauch/harvey/-/harvey-2.5.3.tgz"
+  sha256 "de1ec8e76ef4b8800998f092d3057004e87da92e48ceb1f08bff87048c84bef2"
   license "MIT"
 
   livecheck do
